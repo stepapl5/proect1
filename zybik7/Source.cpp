a9e2fa004939f15200e2512825956422a88f26d9
Student <Student@A303-5>
2025-10-27 19:10:34 +0200

    Добавьте файлы проекта.

---
+++ zybik7/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int maze[4][4] = {
+		 {1, 1, 0, 1},
+		 {0, 1, 1, 1},
+		 {1, 1, 0, 1},
+		 {1, 1, 1, 0}
+	};
+
+	int steps = 0;
+	int x = 0, y = 0;
+
+
+
+
+
+
+
+
+	return 0;
+}
